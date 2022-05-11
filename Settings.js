3586c6d3474579c5a4171f5d6436cf8e33a95e17
GrizzlyStudios <89392649+GrizzlyStudios@users.noreply.github.com>
2022-05-11 08:16:31 -0400

    Add files via upload

--- Settings.js
+++ Settings.js
@@ -1,10 +1,18 @@
 // JavaScript Document
+// query string: ?lol=23
+let lol = getParameterByName("lol");
+document.getElementById("uwu").innerHTML = lol;
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
 document.addEventListener('DOMContentLoaded',() => {
     // handle DOMContentLoaded event
 	loadTheme();
 });
 window.addEventListener('load', function () {
   document.getElementById("loading").style.transform = 	"translate(-50%, -150%)";
+	document.getElementById("loading").style.transition = 	"transform 2s";
 })
 let theme = "theme1";
 let background = "#ffffff";
@@ -22,6 +30,7 @@ function loadTheme(){
 
 function saveTheme()
 {
+	alert(uwu);
 	var themeSave = 
 	{
 		background: "#fff222"
